90316f82500fa72f1883ad02bdc70877eac239e3
Tommy Tran <tran.623@osu.edu>
2021-11-06 21:47:28 -0400

    Add search_crashes to return warning message if current location is within x miles of a previous crash

--- restapi/rest_api.py
+++ restapi/rest_api.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from get_road_name import get_road_name
 from search_crashes import search_crashes
+import pandas as pd
+
+# Load crashclean2.csv dataset into dataframe
+crash_df = pd.read_csv("data/crashclean2.csv", low_memory=False)
+# Columns of interest
+columns = ['LocalReportNumber', 'Latitude', 'Longitude', 'LocationRoadName']
+crash_df = crash_df[columns]
 
 app = Flask(__name__)
 @app.route('/alertme/', methods=['GET', 'POST'])
 def get_warning(points):
     road_names = get_road_name(points[1])
-    warning = search_crashes(road_names)
+    warning = search_crashes(road_names, crash_df, lat, long)
     return warning
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
--- restapi/search_crashes.py
+++ restapi/search_crashes.py
@@ -1,4 +1,34 @@
-import pandas
+import pandas as pd
+from calc_distance import calc_distance
 
-def search_crashes(road_names):
+# Test: delete these later
+# Load crashclean2.csv dataset into dataframe
+test_df = pd.read_csv("data/crashclean2.csv", low_memory=False)
+# Dataframe with only columns of interest
+columns = ['LocalReportNumber', 'Latitude', 'Longitude', 'LocationRoadName']
+test_df = test_df[columns]
+
+def search_crashes(road_names, crash_df, lat, long):
+    # Concatanate road_names into format for parameter in str.contains
+    road_names_str = "|".join(road_names)
+    # Filter rows based on substring of road names
+    filtered_crash_df = crash_df[crash_df["LocationRoadName"].str.contains(road_names_str, case=False, na=False)]
     
+    # Choose crash with x mile radius of current location
+    for index, row in filtered_crash_df.iterrows():
+        crash_lat = row['Latitude']
+        crash_long = row['Longitude']
+        distance = calc_distance(lat, long, crash_lat, crash_long)
+        # max distance (miles) from crash to warn driver
+        distance_threshold = 2
+        warning_msg = "Crash prone zone, please be cautious"
+        if distance < distance_threshold:
+            return warning_msg
+
+    # Delete later
+    # test_df = filtered_crash_df[filtered_crash_df["LocationRoadName"] == "Shrock"]
+    # print(test_df.head(10))
+
+
+# test
+print(search_crashes(["shrock", "broad"], test_df, 39.98525,	-82.79114))
